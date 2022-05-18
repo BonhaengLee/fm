1eff4f7bab9cda7a33ffc889ed8ab2293e6ef1d2
BonhaengLee <bon952014@ajou.ac.kr>
2022-05-18 22:26:28 +0900

    Feat: 메뉴판 만들기

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Base from "./components/Base";
+import Toppings from "./components/Toppings";
+import Order from "./components/Order";
 
 function App() {
+  const [pizza, setPizza] = useState({ base: "", toppings: [] });
+
+  // @ts-ignore
+  const addBase = (base) => {
+    setPizza({ ...pizza, base });
+  };
+  // @ts-ignore
+  const addTopping = (topping) => {
+    let newToppings;
+    // @ts-ignore
+    if (!pizza.toppings.includes(topping)) {
+      newToppings = [...pizza.toppings, topping];
+    } else {
+      newToppings = pizza.toppings.filter((item) => item !== topping);
+    }
+    // @ts-ignore
+    setPizza({ ...pizza, toppings: newToppings });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/newTodo" />
+        <Route
+          path="/base"
+          element={<Base addBase={addBase} pizza={pizza} />}
+        />
+        <Route
+          path="/toppings"
+          element={<Toppings addTopping={addTopping} pizza={pizza} />}
+        />
+        <Route path="/order" element={<Order pizza={pizza} />} />
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
   );
 }
 
