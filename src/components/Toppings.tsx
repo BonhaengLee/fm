62e9377d74e4bcb21ab7122b4a376c44872c89f3
BonhaengLee <bon952014@ajou.ac.kr>
2022-05-20 01:48:48 +0900

    Feat: Variants(2)

--- src/components/Toppings.tsx
+++ src/components/Toppings.tsx
@@ -8,6 +8,21 @@ interface IProps {
   pizza: IPizza;
 }
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: "100vw",
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: {
+      type: "spring",
+      delay: 0.5,
+    },
+  },
+};
+
 const Toppings = ({ addTopping, pizza }: IProps) => {
   const toppings = [
     "mushrooms",
@@ -19,7 +34,12 @@ const Toppings = ({ addTopping, pizza }: IProps) => {
   ];
 
   return (
-    <div className="toppings container">
+    <motion.div
+      className="toppings container"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+    >
       <h3>Step 2: Choose Toppings</h3>
       <ul>
         {toppings.map((topping) => {
@@ -48,7 +68,7 @@ const Toppings = ({ addTopping, pizza }: IProps) => {
           Order
         </motion.button>
       </Link>
-    </div>
+    </motion.div>
   );
 };
 
