fe4e1d90fcfc566d52a6f10cf7ea93ac4e3d5345
BonhaengLee <bon952014@ajou.ac.kr>
2022-05-20 02:13:29 +0900

    Feat: Repeating Animations

--- src/components/Base.tsx
+++ src/components/Base.tsx
@@ -33,6 +33,18 @@ const nextVariants = {
   },
 };
 
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: Infinity,
+    },
+  },
+};
+
 const Base = ({ addBase, pizza }: IProps) => {
   const bases = ["Classic", "Thin & Crispy", "Thick Crust"];
 
@@ -63,13 +75,7 @@ const Base = ({ addBase, pizza }: IProps) => {
       {pizza.base && (
         <motion.div className="next" variants={nextVariants}>
           <Link to="/toppings">
-            <motion.button
-              whileHover={{
-                scale: 1.1,
-                textShadow: "0px 0px 8px rgb(255,255,255)",
-                boxShadow: "0px 0px 8px rgb(255,255,255)",
-              }}
-            >
+            <motion.button variants={buttonVariants} whileHover="hover">
               Next
             </motion.button>
           </Link>
